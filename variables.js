4f6b4e344cf0c86dac6ce241aec92e1665c3138a
Arslan01 <raja.arslanakmal786@gmail.com>
2024-05-17 12:37:45 +0100

    New Files

---
+++ variables.js
@@ -0,0 +1,25 @@
+//Variables
+
+var a = 45;
+let b = "Arslan"
+var c = null
+
+{
+    let b = "Hi"
+    console.log(b)
+}
+console.log(b)
+
+//Arrays
+
+let marks = [100, 90, 80, 70,10, "Arslan"]
+console.log(marks)
+console.log(marks[0])
+console.log(marks[1])
+console.log(marks[2])
+console.log(marks[3])
+console.log(marks[5])
+
+
+
+
