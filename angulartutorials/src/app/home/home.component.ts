a3a5b973e1cf798029d0a3730a66d49cec66cb2c
Arslan01 <raja.arslanakmal786@gmail.com>
2024-06-08 20:14:08 +0100

    Just checking why my folders are not pushing to origin

---
+++ angulartutorials/src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
